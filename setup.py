beae4b97b1fe1cf33aa8c2a46d86b006cde2c3f3
Nastya_Arslanova <narslanova@infoblox.com>
2021-02-17 13:47:49 +0300

    updates for ci

--- setup.py
+++ setup.py
@@ -8,9 +8,8 @@ from setuptools import setup, find_packages
 with open('README.rst') as readme_file:
     readme = readme_file.read()
 
-# with open('HISTORY.rst') as history_file:
-#     history = history_file.read().replace('.. :changelog:', '')
-history = ''
+with open('HISTORY.rst') as history_file:
+    history = history_file.read().replace('.. :changelog:', '')
 
 with open('requirements.txt') as requirements_file:
     requirements = requirements_file.read().splitlines()
