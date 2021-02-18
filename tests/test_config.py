e42dab411e1ee95e3601fe77bde3d01ca5fd5245
Nastya_Arslanova <narslanova@infoblox.com>
2021-02-18 13:33:19 +0300

    updates for ci

--- tests/test_config.py
+++ tests/test_config.py
@@ -8,7 +8,8 @@ class TestConfig(TestCase):
     def setUpClass(klass):
         from importlib import reload
         import sys
-        config = reload(sys.modules['config'])
+        # config = reload(sys.modules['config'])
+        print(sys.modules)
         print('here')
         config.config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)),
                                           "test_config.json")
